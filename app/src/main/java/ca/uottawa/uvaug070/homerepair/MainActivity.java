aee8ba837bb459d60a095b8270b6f34952dff54c
UVaughan <uvaug070@uottawa.ca>
2018-10-23 11:43:49 -0400

    Initial commit
    Created UI

---
+++ app/src/main/java/ca/uottawa/uvaug070/homerepair/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.uottawa.uvaug070.homerepair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
