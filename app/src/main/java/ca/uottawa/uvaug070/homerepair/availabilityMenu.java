dc099375533afcf976dc62d3b128f2131a1bb671
adenli <ali128@uottawa.ca>
2018-11-17 02:09:57 -0500

    added spinners

--- app/src/main/java/ca/uottawa/uvaug070/homerepair/availabilityMenu.java
+++ app/src/main/java/ca/uottawa/uvaug070/homerepair/availabilityMenu.java
@@ -1,25 +1,26 @@
 package ca.uottawa.uvaug070.homerepair;
 
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
-import android.widget.Toast;
 
-import com.mcsoft.timerangepickerdialog.RangeTimePickerDialog;
+import java.util.Calendar;
 
-public class availabilityMenu extends Fragment implements RangeTimePickerDialog.ISelectedTime {
+public class availabilityMenu extends Fragment {
 
 
-   private Boolean switchState;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,6 +28,26 @@ public class availabilityMenu extends Fragment implements RangeTimePickerDialog.
         //change R.layout.yourlayoutfilename for each of your fragments
         View view = inflater.inflate(R.layout.fragment_availability, container, false);
         super.onCreate(savedInstanceState);
+        Spinner spin = (Spinner) view.findViewById(R.id.spinner);
+        Spinner spin1= view.findViewById(R.id.spinner1);
+        Spinner spin2= view.findViewById(R.id.spinner2);
+        Spinner spin3= view.findViewById(R.id.spinner3);
+        Spinner spin4= view.findViewById(R.id.spinner4);
+        Spinner spin5= view.findViewById(R.id.spinner5);
+        Spinner spin6= view.findViewById(R.id.spinner6);
+        String[] Spinnerlist={"Opening time","Closing time"};
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getActivity(),R.layout.support_simple_spinner_dropdown_item,Spinnerlist);
+        spin.setAdapter(arrayAdapter);
+        spin.setEnabled(false);
+        spin1.setEnabled(false);
+        spin2.setEnabled(false);
+        spin3.setEnabled(false);
+        spin4.setEnabled(false);
+        spin5.setEnabled(false);
+        spin6.setEnabled(false);
+
+        final EditText ass = (EditText) getActivity().findViewById(R.id.ass);
 
         return view;
         //this will initialize the layout of the activity servproaddserv
@@ -40,72 +61,291 @@ public class availabilityMenu extends Fragment implements RangeTimePickerDialog.
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Calendar");
 
+    }
 
+    @Override
+    public void onStart() {
+
+        super.onStart();
+        Spinner spin = (Spinner) getActivity().findViewById(R.id.spinner);
         Switch simpleSwitch = (Switch) getActivity().findViewById(R.id.monday);
-        Switch simpleSwitch1 = (Switch) getActivity().findViewById(R.id.tuesday);
-        Switch simpleSwitch2 = (Switch) getActivity().findViewById(R.id.wednesday);
-        Switch simpleSwitch3 = (Switch) getActivity().findViewById(R.id.thursday);
-        Switch simpleSwitch4 = (Switch) getActivity().findViewById(R.id.friday);
-        Switch simpleSwitch5 = (Switch) getActivity().findViewById(R.id.saturday);
-        Switch simpleSwitch6 = (Switch) getActivity().findViewById(R.id.sunday);
-
-        final EditText ass = (EditText) getActivity().findViewById(R.id.ass);
-
-//        final Boolean switchState = simpleSwitch.isChecked(); //0 being monday and 6 being sunday
-//        Boolean switchState1 = simpleSwitch1.isChecked();
-//        Boolean switchState2 = simpleSwitch2.isChecked();
-//        Boolean switchState3 = simpleSwitch3.isChecked();
-//        Boolean switchState4 = simpleSwitch4.isChecked();
-//        Boolean switchState5 = simpleSwitch5.isChecked();
-//        Boolean switchState6 = simpleSwitch6.isChecked();
-//        simpleSwitch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                if (switchState==false){
-//
-//                    Toast.makeText(getActivity().getApplicationContext(), "niggers", Toast.LENGTH_SHORT).show();
-//                }
-//
-//            }
-//        });
 
         simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            Spinner spin = (Spinner) getActivity().findViewById(R.id.spinner);
+
+            final EditText ass = (EditText) getActivity().findViewById(R.id.ass);
             @SuppressLint("ResourceType")
             @Override
             public void onCheckedChanged(CompoundButton cb, boolean on){
                 if(on) {
 
+                    spin.setEnabled(true);
                     // Get Current Time
-//                    final Calendar c = Calendar.getInstance();
-//                    int mHour = c.get(Calendar.HOUR_OF_DAY);
-//                    int mMinute = c.get(Calendar.MINUTE);
-//                    DialogFragment newFragment = new timepickerfragment();
-//                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
-                    RangeTimePickerDialog dialog = new RangeTimePickerDialog();
-                    dialog.newInstance();
-                    dialog.setRadiusDialog(20); // Set radius of dialog (default is 50)
-                    dialog.setIs24HourView(true); // Indicates if the format should be 24 hours
-                    dialog.setColorBackgroundHeader(R.color.colorPrimary); // Set Color of Background header dialog
-                    dialog.setColorTextButton(R.color.colorPrimaryDark); // Set Text color of button
-                    FragmentManager fragmentManager = getFragmentManager();
+                    final Calendar c = Calendar.getInstance();
+                    int mHour = c.get(Calendar.HOUR_OF_DAY);
+                    int mMinute = c.get(Calendar.MINUTE);
 
-                    dialog.show(getActivity().getFragmentManager(),"");
+                    spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            Object value = parent.getItemAtPosition(position);
+                            switch (position) {
+                                case 0:
+                                    DialogFragment newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                                case 1:
+                                    newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                            }
 
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                            
+                        }
+
+                });
                 }
-                else
-                {
-                    //Do something when Switch is off/unchecked
 
+                else{
+                    spin.setEnabled(false);
+                }
+            }
+        });
+        Spinner spin1 = (Spinner) getActivity().findViewById(R.id.spinner1);
+        Switch simpleSwitch1 = (Switch) getActivity().findViewById(R.id.tuesday);
+        simpleSwitch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            Spinner spin1 = (Spinner) getActivity().findViewById(R.id.spinner1);
+
+            final EditText ass = (EditText) getActivity().findViewById(R.id.ass);
+            @SuppressLint("ResourceType")
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on){
+                if(on) {
+
+                    spin1.setEnabled(true);
+                    // Get Current Time
+                    final Calendar c = Calendar.getInstance();
+                    int mHour = c.get(Calendar.HOUR_OF_DAY);
+                    int mMinute = c.get(Calendar.MINUTE);
+
+                    spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            Object value = parent.getItemAtPosition(position);
+                            switch (position) {
+                                case 0:
+                                    DialogFragment newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                                case 1:
+                                    newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                            }
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+
+                    });
+                }
+
+                else{
+                    spin1.setEnabled(false);
                 }
             }
         });
 
-    }
+        Spinner spin2 = (Spinner) getActivity().findViewById(R.id.spinner2);
+        Switch simpleSwitch2 = (Switch) getActivity().findViewById(R.id.wednesday);
+        simpleSwitch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            Spinner spin2 = (Spinner) getActivity().findViewById(R.id.spinner);
 
+            final EditText ass = (EditText) getActivity().findViewById(R.id.ass);
+            @SuppressLint("ResourceType")
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on){
+                if(on) {
 
-    @Override
-    public void onSelectedTime(int i, int i1, int i2, int i3) {
-        Toast.makeText(getActivity(), "Start", Toast.LENGTH_SHORT).show();
+                    spin2.setEnabled(true);
+                    // Get Current Time
+                    final Calendar c = Calendar.getInstance();
+                    int mHour = c.get(Calendar.HOUR_OF_DAY);
+                    int mMinute = c.get(Calendar.MINUTE);
+
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            Object value = parent.getItemAtPosition(position);
+                            switch (position) {
+                                case 0:
+                                    DialogFragment newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                                case 1:
+                                    newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                            }
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+
+                    });
+                }
+
+                else{
+                    spin2.setEnabled(false);
+                }
+            }
+        });
+        Spinner spin3 = (Spinner) getActivity().findViewById(R.id.spinner3);
+        Switch simpleSwitch3 = (Switch) getActivity().findViewById(R.id.thursday);
+        simpleSwitch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            Spinner spin3 = (Spinner) getActivity().findViewById(R.id.spinner3);
+
+            final EditText ass = (EditText) getActivity().findViewById(R.id.ass);
+            @SuppressLint("ResourceType")
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on){
+                if(on) {
+
+                    spin3.setEnabled(true);
+                    // Get Current Time
+                    final Calendar c = Calendar.getInstance();
+                    int mHour = c.get(Calendar.HOUR_OF_DAY);
+                    int mMinute = c.get(Calendar.MINUTE);
+
+                    spin3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            Object value = parent.getItemAtPosition(position);
+                            switch (position) {
+                                case 0:
+                                    DialogFragment newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                                case 1:
+                                    newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                            }
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+
+                    });
+                }
+
+                else{
+                    spin3.setEnabled(false);
+                }
+            }
+        });
+
+        Spinner spin4 = (Spinner) getActivity().findViewById(R.id.spinner4);
+        Switch simpleSwitch4 = (Switch) getActivity().findViewById(R.id.friday);
+        simpleSwitch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            Spinner spin4 = (Spinner) getActivity().findViewById(R.id.spinner4);
+
+            final EditText ass = (EditText) getActivity().findViewById(R.id.ass);
+            @SuppressLint("ResourceType")
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on){
+                if(on) {
+
+                    spin4.setEnabled(true);
+                    // Get Current Time
+                    final Calendar c = Calendar.getInstance();
+                    int mHour = c.get(Calendar.HOUR_OF_DAY);
+                    int mMinute = c.get(Calendar.MINUTE);
+
+                    spin4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            Object value = parent.getItemAtPosition(position);
+                            switch (position) {
+                                case 0:
+                                    DialogFragment newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                                case 1:
+                                    newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                            }
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+
+                    });
+                }
+
+                else{
+                    spin4.setEnabled(false);
+                }
+            }
+        });
+
+        Spinner spin5 = (Spinner) getActivity().findViewById(R.id.spinner5);
+        Switch simpleSwitch5 = (Switch) getActivity().findViewById(R.id.saturday);
+        simpleSwitch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            Spinner spin5 = (Spinner) getActivity().findViewById(R.id.spinner5);
+
+            final EditText ass = (EditText) getActivity().findViewById(R.id.ass);
+            @SuppressLint("ResourceType")
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on){
+                if(on) {
+
+                    spin5.setEnabled(true);
+                    // Get Current Time
+                    final Calendar c = Calendar.getInstance();
+                    int mHour = c.get(Calendar.HOUR_OF_DAY);
+                    int mMinute = c.get(Calendar.MINUTE);
+
+                    spin5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            Object value = parent.getItemAtPosition(position);
+                            switch (position) {
+                                case 0:
+                                    DialogFragment newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                                case 1:
+                                    newFragment = new timepickerfragment();
+                                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                                    break;
+                            }
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+
+                    });
+                }
+
+                else{
+                    spin5.setEnabled(false);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/ca/uottawa/uvaug070/homerepair/timepickerfragment.java
+++ app/src/main/java/ca/uottawa/uvaug070/homerepair/timepickerfragment.java
@@ -5,7 +5,11 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.format.DateFormat;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
@@ -23,12 +27,22 @@ public class timepickerfragment extends DialogFragment
         TimePickerDialog a = new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
         // Create a new instance of TimePickerDialog and return it
 
-        a.setTitle("penis");
+        TextView tv = new TextView(getActivity());
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
+                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+        tv.setLayoutParams(lp);
+        tv.setPadding(10, 10, 10, 10);
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP,20);
+        tv.setText("This is a custom title.");
+        a.setCustomTitle(tv);
         return a;
     }
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         EditText tv1=(EditText) getActivity().findViewById(R.id.ass);
+
         tv1.setText("Hour: "+view.getCurrentHour()+" Minute: "+view.getCurrentMinute());
 
     }
