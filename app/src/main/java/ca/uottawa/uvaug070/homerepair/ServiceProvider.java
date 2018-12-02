04c2d828f8fd747500677e4ecd8af32eb7427daf
ulysses vaughan <uvaug070@uottawa.ca>
2018-12-02 15:56:37 -0500

    Update ServiceProvider.java

--- app/src/main/java/ca/uottawa/uvaug070/homerepair/ServiceProvider.java
+++ app/src/main/java/ca/uottawa/uvaug070/homerepair/ServiceProvider.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class ServiceProvider extends Account {
     private Profile myProfile;
+    private int numRatings;
+    private int cumulativeRating;
     public ServiceProvider(String username, String password, Role role, String uid) {
         super(username, password, role, uid);
     }
@@ -16,4 +18,23 @@ public class ServiceProvider extends Account {
     }
     public Profile getServiceProviderProfile(){return myProfile;}
     public void setServiceProviderProfile(Profile newProfile){this.myProfile=newProfile;}
+    public void rate(int rating) {
+        cumulativeRating += rating;
+        numRatings++;
+    }
+    public double getRating() {
+        return (double)cumulativeRating/(double)numRatings;
+    }
+    public int getNumRatings() {
+        return numRatings;
+    }
+    public void setNumRatings(int numRatings) {
+        this.numRatings = numRatings;
+    }
+    public int getCumulativeRating() {
+        return cumulativeRating;
+    }
+    public void setCumulativeRating(int cumulativeRating) {
+        this.cumulativeRating = cumulativeRating;
+    }
 }
